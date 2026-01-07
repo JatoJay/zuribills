34baaaeca9d51a4b88d4213957b260a4af534874
Jato Joseph <jatofanan@gmail.com>
2026-01-07 15:20:20 +0200

    modified:src/pages/Landing.tsx

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-0d7dea0d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.3ts45122p2g"
+    "revision": "0.8p1q7d2dohg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
