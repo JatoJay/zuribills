59ffd3dc27b450a45ce1f897da003251f2851d45
Jato Joseph <jatofanan@gmail.com>
2026-03-30 17:43:39 +0200

    fix: use to field for email

--- src/services/paymentService.ts
+++ src/services/paymentService.ts
@@ -79,7 +79,7 @@ export const initPolarPayment = async (
                 invoiceId: config.invoiceId,
                 amount: config.amount,
                 currency: config.currency,
-                contact: config.customerEmail,
+                to: config.customerEmail,
                 name: config.customerName,
                 description: config.description,
             }),
